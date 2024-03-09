b4e4f5376f929817351d15cf23589c3cd4add46e
vazhaberdzenishvili <vazha.berdzenishvili.1@iliauni.edu.ge>
2024-03-09 10:17:02 +0400

    Add tag model

--- app/core/models.py
+++ app/core/models.py
@@ -52,9 +52,23 @@ class Recipe(models.Model):
         on_delete=models.CASCADE,
     )
     title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
     time_minutes = models.IntegerField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
     link = models.CharField(max_length=255, blank=True)
+    tags = models.ManyToManyField('Tag')
 
     def __str__(self):
         return self.title
+
+
+class Tag(models.Model):
+    """Tag for filtering recipes."""
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.name
