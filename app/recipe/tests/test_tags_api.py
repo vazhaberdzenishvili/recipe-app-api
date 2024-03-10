e7aa3dc25d9d19feee3f21a8ca6cd101fdb80ca4
vazhaberdzenishvili <vazha.berdzenishvili.1@iliauni.edu.ge>
2024-03-10 11:52:43 +0400

    fix linting errors

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -15,9 +15,11 @@ from recipe.serializers import TagSerializer
 
 TAGS_URL = reverse('recipe:tag-list')
 
+
 def detail_url(tag_id):
     """Create and return a tag detail url."""
     return reverse('recipe:tag-detail', args=[tag_id])
+
 
 def create_user(email='user@example.com', password='testpass123'):
     """Create and return a user."""
@@ -71,13 +73,13 @@ class PRivateTagsApiTests(TestCase):
         self.assertEqual(res.data[0]['id'], tag.id)
 
     def test_update_tag(self):
-            """Test updating a tag."""
-            tag = Tag.objects.create(user=self.user, name='After Dinner')
+        """Test updating a tag."""
+        tag = Tag.objects.create(user=self.user, name='After Dinner')
 
-            payload = {'name': 'Dessert'}
-            url = detail_url(tag.id)
-            res = self.client.patch(url, payload)
+        payload = {'name': 'Dessert'}
+        url = detail_url(tag.id)
+        res = self.client.patch(url, payload)
 
-            self.assertEqual(res.status_code, status.HTTP_200_OK)
-            tag.refresh_from_db()
-            self.assertEqual(tag.name, payload['name'])
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        tag.refresh_from_db()
+        self.assertEqual(tag.name, payload['name'])
